5517765f52e83165018aeb8dbfaf0a61bd763ce0
vaibhav <vaibhavmehta524@gmail.com>
2023-03-10 05:06:52 +0530

    Modified to Response Happy Mood Test Case

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -13,5 +13,12 @@ namespace MoodAnalyserTest
             Assert.AreEqual(result, "Sad");
             Assert.Pass();
         }
+        [Test]
+        public void GivenHappyMood_WhenAnalysed_SholudReturnHappy()
+        {
+            string result = moodAnalyser.AnalyseMood("I am in Happy Mood");
+            Assert.AreEqual(result, "Happy");
+            Assert.Pass();
+        }
     }
 }
