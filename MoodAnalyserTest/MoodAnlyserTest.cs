4ee27bf056d436b88357ad3ab3fec06eb6efdf6c
vaibhav <vaibhavmehta524@gmail.com>
2023-03-10 05:28:40 +0530

    Modified to Refactor UC1 To Use Parameterised Constructor Test Case

--- MoodAnalyserTest/MoodAnlyserTest.cs
+++ MoodAnalyserTest/MoodAnlyserTest.cs
@@ -13,5 +13,13 @@ namespace MoodAnalyserTest
             Assert.AreEqual(result, "Sad");
             Assert.Pass();
         }
+        [Test]
+        public void GivenSadMood_WhenAnalysed_SholudReturnHappy()
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+            string result = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(result, "Happy");
+            Assert.Pass();
+        }
     }
 }
